bd467edfb9afa37d04fe4322cafc644af1436ffb
Thomas Thorsager <t-thorsager@hotmail.com>
2018-10-18 09:39:50 +0200

    Guidelabel added to Color Xylophone. Change of backgroundcolor when buttons/keys tapped in Color Xylophone. Sound Credits font color turned white in Color Xylophone

--- Colors/ColorXylophoneViewController.swift
+++ Colors/ColorXylophoneViewController.swift
@@ -4,7 +4,7 @@
 //
 //  Created by Thomas Thorsager on 15/10/2018.
 //  Copyright © 2018 TThorsager. All rights reserved.
-//
+//  Sounds from Freesound.org/people/dwengomes
 
 import UIKit
 import AVFoundation
@@ -24,9 +24,11 @@ class ColorXylophoneViewController: UIViewController, AVAudioPlayerDelegate {
     @IBOutlet weak var orangeKey: UIButton!
     @IBOutlet weak var yellowKey: UIButton!
     
+    @IBOutlet weak var guideLabel: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        guideLabel.text = "Tap a button to change the color"
         
         greenKey.layer.cornerRadius = 20
         tealKey.layer.cornerRadius = 20
@@ -40,12 +42,36 @@ class ColorXylophoneViewController: UIViewController, AVAudioPlayerDelegate {
     
     
     @IBAction func keyTapped(_ sender: UIButton) {
-
+        guideLabel.text = ""
+        
         soundNameSelected = soundArray[sender.tag - 1]
         
         print(soundNameSelected)
         
         playSound()
+        
+        //Altering the backgroundcolor to nearly match the color of the button/key tapped
+        if sender.tag == 1 {
+            view.layer.backgroundColor = #colorLiteral(red: 0.462745098, green: 0.8980392157, blue: 0.4823529412, alpha: 1)
+        } else if sender.tag == 2 {
+            view.layer.backgroundColor = #colorLiteral(red: 0.4274509804, green: 0.9882352941, blue: 0.8823529412, alpha: 1)
+        } else if sender.tag == 3 {
+            view.layer.backgroundColor = #colorLiteral(red: 0.4431372549, green: 0.6705882353, blue: 0.8784313725, alpha: 1)
+        } else if sender.tag == 4 {
+            view.layer.backgroundColor = #colorLiteral(red: 0.7294117647, green: 0.3529411765, blue: 0.8862745098, alpha: 1)
+        } else if sender.tag == 5 {
+            view.layer.backgroundColor = #colorLiteral(red: 0.8784313725, green: 0.3529411765, blue: 0.3529411765, alpha: 1)
+        } else if sender.tag == 6 {
+            view.layer.backgroundColor = #colorLiteral(red: 0.8980392157, green: 0.5725490196, blue: 0.3529411765, alpha: 1)
+        } else if sender.tag == 7 {
+            view.layer.backgroundColor = #colorLiteral(red: 0.8862745098, green: 0.8862745098, blue: 0.2235294118, alpha: 1)
+        } 
+        
+        
+        
+        
+        
+        
         
     }
     
